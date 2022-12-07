5b580b18a09a7ec1bd3678b3e3854150b5687291
sakimiya <saki.miyajima@lasfactory.com>
2022-12-07 11:17:05 +0900

    extension用export、manifest.json作成

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    images: {
+      unoptimized: true, // Imageタグをexport可能にする
+    },
+  },
+  assetPrefix: "./", // 画像、cssなどを反映させる
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
